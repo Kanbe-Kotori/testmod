867ceafd6a43049db32878deaf99c9b0aa7ca556
Kanbe-Kotori <1438726022@qq.com>
2022-07-04 01:55:12 +0800

    fix

--- src/main/java/cn/nulladev/testmod/MagicRender.java
+++ src/main/java/cn/nulladev/testmod/MagicRender.java
@@ -27,13 +27,9 @@ public class MagicRender extends EntityRenderer<EntityMagic> {
         if (entity == null)
             return;
 
-        RenderSystem.enableBlend();
-        RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-
         stack.pushPose();
-        stack.mulPose(Vector3f.YP.rotationDegrees(entity.getXRot() - 90.0F));
-        stack.mulPose(Vector3f.ZP.rotationDegrees(entity.getYRot()));
+        //stack.mulPose(Vector3f.YP.rotationDegrees(entity.getXRot() - 90.0F));
+        //stack.mulPose(Vector3f.ZP.rotationDegrees(entity.getYRot()));
 
         double d = 0.5D;
         double r1 = 2D;
@@ -41,14 +37,14 @@ public class MagicRender extends EntityRenderer<EntityMagic> {
 
         int gradation = 30;
         VertexConsumer vc = buffer.getBuffer(TestRenderType.trinagleStrip());
+        vc.vertex(0, 0, 1).color(1F, 1F, 1F, 1F).endVertex();
+        vc.vertex(1, 0, 0).color(1F, 1F, 1F, 1F).endVertex();
+        vc.vertex(0, 1, 0).color(1F, 1F, 1F, 1F).endVertex();
         /*for (int i = 0; i < gradation; ++i) {
             stack.mulPose(Vector3f.XP.rotationDegrees(360.0F / gradation));
             vc.vertex(d, r1, 0).color(1F, 1F, 1F, 1F).endVertex();
             vc.vertex(d, r1 - w1, 0).color(1F, 1F, 1F, 1F).endVertex();
         }*/
-        vc.vertex(0, 0, 0).color(1F, 1F, 1F, 1F).endVertex();
-        vc.vertex(1, 0, 0).color(1F, 1F, 1F, 1F).endVertex();
-        vc.vertex(0, 1, 0).color(1F, 1F, 1F, 1F).endVertex();
 
         stack.popPose();
     }
